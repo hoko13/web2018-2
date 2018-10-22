81239b55437ab6966ed5ad3d0520bde05b25230e
zm17419 <zm17419@josai.ac.jp>
2018-10-21 21:53:29 -0400

    message

---
+++ ex06-8.js
@@ -0,0 +1,7 @@
+function sayhello68(){
+  alert('Hello, + document.getElementByld("who68").innerHTML + "!");}
+function sayhello68(){
+  document.getElementByld("who68").innerHTML ="太郎";}
+function sayhello68(){
+  document.getElementByld("who68").innerHTML ="花子";}
+                          
