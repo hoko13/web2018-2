878086f2e6290cfebf5803217560974c0557b32c
zm17419 <zm17419@josai.ac.jp>
2019-02-03 08:03:20 -0500

    message

--- ex6-7.js
+++ ex6-7.js
@@ -1,8 +1,8 @@
 var who67='world';
 function sayhello67(){ 
 alert('Hello,' + who67 + '!');}
-function taro67(){ who67 ='太郎';}
-function hanako67(){ who67 ='花子';}
-function rei67(){ who67 ='レイ';}
-function sinzi67(){ who67 ='シンジ';}
-function asuka67(){ who67 ='アスカ';}
+function taro67(){ who67 ='えびふらい';}
+function hanako67(){ who67 ='たまご';}
+function rei67(){ who67 ='ぶぶ';}
+function sinzi67(){ who67 ='あき';}
+function asuka67(){ who67 ='ふゆ';}
